8b7fbebccc193392635800351eb721d22e8c1ee6
francito69 <franz.inga.c@uni.pe>
2025-12-19 14:47:17 -0500

    update todo

--- frontend/src/services/api/ordenService.js
+++
@@ -1,8 +0,0 @@
-// frontend/src/services/api/ordenService.js
-import api from './config';
-
-export const ordenService = {
-  crearOrden: (ordenData) => api.post('/ordenes', ordenData),
-  obtenerOrdenes: () => api.get('/ordenes'),
-  obtenerOrdenPorId: (id) => api.get(`/ordenes/${id}`)
-};
